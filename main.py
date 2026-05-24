6dc30171cfad91fb979bd4fb400ebc72999cb384
KevinQqs <ksquinones09@ucatolica.edu.co>
2026-05-24 15:59:32 -0500

    (Fix) main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routers import movies, directors, genres
 from app.db import crear_tablas
 
@@ -15,3 +17,9 @@ def on_startup():
 app.include_router(movies.router)
 app.include_router(directors.router)
 app.include_router(genres.router)
+
+app.mount("/static", StaticFiles(directory="."), name="static")
+
+@app.get("/")
+def index():
+    return FileResponse("index.html")
